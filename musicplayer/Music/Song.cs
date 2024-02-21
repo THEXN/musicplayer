e79c9e26bc0617d5b50c82a3b4abdc923729d314
xien <2383759126@qq.com>
2024-02-22 02:11:07 +0800

    简洁

---
+++ musicplayer/Music/Song.cs
@@ -0,0 +1,90 @@
+﻿using IL.Terraria.ID;
+using On.Terraria.ID;
+using System;
+using System.Collections.Generic;
+using Terraria.ID;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace MusicPlayer.Music
+{
+    internal class Song
+    {
+        public string Name { get; set; }
+        public TSPlayer Player { get; set; } // 添加 Player 属性
+        public Song(TerrariaPlugin plugin, string name, string path, SongPlayer ply)
+        {
+            this.Name = name;
+            this.notesToPlay = NoteFileParser.Read(path, out this.tempo);
+            this.songLength = this.notesToPlay.Count;
+            this.player = ply;
+            this.plugin = plugin;
+        }
+        public void Update(EventArgs args)
+        {
+            if (this.isPlaying)
+            {
+                if (this.notesToPlay.Count == 0)
+                {
+                    this.EndSong();
+                }
+                else
+                {
+                    this.delta += (int)(DateTime.Now - this.lastUpdate).TotalMilliseconds;
+
+                    if (this.delta > this.tempo)
+                    {
+                        List<Note> list = this.notesToPlay[0];
+                        this.notesToPlay.RemoveAt(0);
+
+                        foreach (Note note in list)
+                        {
+                            float? noteValue = note.Value;
+                            if (noteValue >= -1f && noteValue <= 1f)
+                            {
+                                this.PlayNote(noteValue.Value);
+                            }
+
+                        }
+
+                        this.delta -= this.tempo;
+                    }
+
+                    this.currentPlayTime += this.delta;
+                    this.lastUpdate = DateTime.Now;
+                }
+            }
+        }
+
+
+        public void StartSong()
+        {
+            this.isPlaying = true;
+            this.player.Listening = true;
+            this.currentPlayTime = 0;
+            this.lastUpdate = DateTime.Now;
+            ServerApi.Hooks.GamePostUpdate.Register(this.plugin, new HookHandler<EventArgs>(this.Update));
+        }
+        public void EndSong()
+        {
+            this.isPlaying = false;
+            ServerApi.Hooks.GamePostUpdate.Deregister(this.plugin, new HookHandler<EventArgs>(this.Update));
+            this.player.Listening = false;
+        }
+        private void PlayNote(float note)
+        {
+            this.Player.SendData((PacketTypes)58, "", this.Player.Index, note, 0f, 0f, 0); // 修改为 this.Player
+        }
+        private List<List<Note>> notesToPlay = new List<List<Note>>();
+        private SongPlayer player;
+        private int currentPlayTime = 0;
+        private bool isPlaying = false;
+        private TerrariaPlugin plugin;
+        private int tempo = NoteFileParser.Tempo;
+
+        private int songLength = 0;
+
+        private int delta = 0;
+        private DateTime lastUpdate = DateTime.Now;
+    }
+}
---
+++ musicplayer/SongPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using MusicPlayer.Music;
+using TShockAPI;
+
+namespace MusicPlayer
+{
+    internal class SongPlayer
+    {
+        public bool Listening { get; set; }
+        public TSPlayer Player { get; set; }
+        public Song currentSong = null;
+
+        public SongPlayer(TSPlayer ply)
+        {
+            this.Player = ply;
+            this.Listening = false;
+        }
+
+        public void StartSong(Song s)
+        {
+            if (this.currentSong != null)
+            {
+                this.currentSong.EndSong();
+            }
+            this.currentSong = s;
+
+            // 只为执行命令的玩家设置 Player 属性
+            s.Player = this.Player;
+            s.StartSong();
+        }
+
+        public void StartSongAll(Song s)
+        {
+            foreach (TSPlayer player in TShock.Players)
+            {
+                if (player?.Active ?? false)
+                {
+                    s.Player = player; // 设置当前播放歌曲的玩家
+                    s.StartSong(); // 开始播放歌曲
+                }
+            }
+        }
+
+
+
+        public void EndSong()
+        {
+            if (this.currentSong != null)
+            {
+                this.currentSong.EndSong();
+            }
+            this.currentSong = null;
+        }
+    }
+
+}
