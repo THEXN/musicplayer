45cb29eca2256ae9afd5c6b0956be97bcaff9613
xien <2383759126@qq.com>
2024-05-16 22:36:08 +0800

    防止有傻呗在服务器后台执行

--- musicplayer/MusicPlayer.cs
+++ musicplayer/MusicPlayer.cs
@@ -33,8 +33,16 @@ namespace MusicPlayer
 
         public override void Initialize()
         {
-            Commands.ChatCommands.Add(new Command("song", PlaySong, "song"));
-            Commands.ChatCommands.Add(new Command("song2", PlaySongAll, "song2"));
+            Commands.ChatCommands.Add(new Command("song", PlaySong, "song")
+            {
+                HelpText = "返回最后一次死亡的位置",
+                AllowServer = false
+            });
+            Commands.ChatCommands.Add(new Command("song2", PlaySongAll, "song2")
+            {
+                HelpText = "返回最后一次死亡的位置",
+                AllowServer = false
+            });
 
             ServerApi.Hooks.NetGreetPlayer.Register(this, OnJoin);
             ServerApi.Hooks.ServerLeave.Register(this, OnLeave);
