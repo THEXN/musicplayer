57a9b841d3bc371c5cb018550e8b0a4af28fccb0
xien <2383759126@qq.com>
2024-06-09 00:06:28 +0800

    还原

--- musicplayer/MusicPlayer.cs
+++ musicplayer/MusicPlayer.cs
@@ -15,7 +15,7 @@ namespace MusicPlayer
 
         public override string Name => "音乐播放器";
 
-        public override Version Version => new Version(1, 0, 0, 1);
+        public override Version Version => new Version(1, 0, 2);
 
         public string songPath;
 
